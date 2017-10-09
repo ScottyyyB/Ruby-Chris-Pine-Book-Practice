97940f784dffd3411f856a84bbd42781720a3943
Scott <17sb@cis.dk>
2017-10-09 16:21:13 +0200

    Finished Variables & Assignment (Chap3)

--- calc.rb
+++ calc.rb
@@ -6,6 +6,7 @@ puts (60 * 60) * 24 * (365 * 18)
 =end
 
 # String Practice
+=begin
 puts "Hello, world!"
 puts ""
 puts "Good-bye!"
@@ -15,3 +16,29 @@ puts 'You\'re swell!'
 puts 'backslash at the end of a string \\'
 puts 'up\\down'
 puts 'up\down'
+=end
+
+#Variables & Assignment Practice
+my_string = '...you can say that again...'
+puts my_string
+puts my_string
+
+name = "Bojo Joey BoBo"
+puts "My name is #{name}."
+puts "Wow! #{name} is a really long name!"
+name = "Ralph"
+puts "My name has changd to #{name}!"
+var = "just another " + "string"
+puts var
+
+var = 5 * (1+2)
+puts var
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+puts ''
+
+var1 = 'eight'
+puts var1
+puts var2
