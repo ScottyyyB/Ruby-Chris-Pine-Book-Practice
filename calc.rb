864f9633ee6ccc41f9ea4dc24c2cb9e8a171481c
Scott <17sb@cis.dk>
2017-10-09 16:03:27 +0200

    Finished Numbers (Chap2)

--- calc.rb
+++ calc.rb
@@ -1,5 +1,17 @@
-
+=begin
 puts 5 * (12 - 8) + -15
 puts 24 * 365
 puts (24 * 365) * 10
 puts (60 * 60) * 24 * (365 * 18)
+=end
+
+# String Practice
+puts "Hello, world!"
+puts ""
+puts "Good-bye!"
+friend = "Seb"
+puts "Hello, #{friend}! " * 4
+puts 'You\'re swell!'
+puts 'backslash at the end of a string \\'
+puts 'up\\down'
+puts 'up\down'
